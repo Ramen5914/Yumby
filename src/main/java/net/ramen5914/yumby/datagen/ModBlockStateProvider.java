73fec55826c8f6d5edc1cf266ad3c0edb96d57aa
Omar Rahman <omar+github@ramen5914.net>
2024-11-08 15:46:41 -0800

    Added the blockstates provider

---
+++ src/main/java/net/ramen5914/yumby/datagen/ModBlockStateProvider.java
@@ -0,0 +1,20 @@
+package net.ramen5914.yumby.datagen;
+
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
+import net.neoforged.neoforge.client.model.generators.ModelFile;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.ramen5914.yumby.Yumby;
+import net.ramen5914.yumby.block.ModBlocks;
+
+public class ModBlockStateProvider extends BlockStateProvider {
+    public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
+        super(output, Yumby.MOD_ID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+        simpleBlockWithItem(ModBlocks.POT.get(),
+                new ModelFile.UncheckedModelFile(modLoc("block/pot")));
+    }
+}
