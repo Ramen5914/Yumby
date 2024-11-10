92a9c75ed9deb7da313972ae02608effd24c0519
Omar Rahman <omar+github@ramen5914.net>
2024-11-10 08:12:18 -0800

    Added new hitboxed for all rotations of pan

--- src/main/java/net/ramen5914/yumby/block/custom/PanBlock.java
+++ src/main/java/net/ramen5914/yumby/block/custom/PanBlock.java
@@ -7,6 +7,23 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
+    public static final MapCodec<PanBlock> CODEC = simpleCodec(PanBlock::new);
+    public static final EnumProperty<Direction> FACING = BlockStateProperties.HORIZONTAL_FACING;
+
+    private static final VoxelShape NORTH_HANDLE = Block.box(9.25, 1, 7, 15.25, 2, 9);
+    private static final VoxelShape EAST_HANDLE = Block.box(7, 1, 9.25, 9, 2, 15.25);
+    private static final VoxelShape SOUTH_HANDLE = Block.box(0.75, 1, 7, 6.75, 2, 9);
+    private static final VoxelShape WEST_HANDLE = Block.box(7, 1, 0.75, 9, 2, 6.75);
+
+    private static final VoxelShape NORTH_DISH = Block.box(0.75, 0, 3.75, 9.25, 2, 12.25);
+    private static final VoxelShape EAST_DISH = Block.box(3.75, 0, 0.75, 12.25, 2, 9.25);
+    private static final VoxelShape SOUTH_DISH = Block.box(6.75, 0, 3.75, 15.25, 2, 12.25);
+    private static final VoxelShape WEST_DISH = Block.box(3.75, 0, 6.75, 12.25, 2, 15.25);
+
+    private static final VoxelShape NORTH_PAN = Shapes.or(NORTH_DISH, NORTH_HANDLE);
+    private static final VoxelShape EAST_PAN = Shapes.or(EAST_DISH, EAST_HANDLE);
+    private static final VoxelShape SOUTH_PAN = Shapes.or(SOUTH_DISH, SOUTH_HANDLE);
+    private static final VoxelShape WEST_PAN = Shapes.or(WEST_DISH, WEST_HANDLE);
 
 public class PanBlock extends Block {
     public PanBlock(Properties properties) {
