c869726e541471fb59009bed3dfa04ff48d29039
Omar Rahman <omar+github@ramen5914.net>
2024-11-10 08:33:57 -0800

    Added pot recipe to crafting table

--- src/main/java/net/ramen5914/yumby/datagen/ModRecipeProvider.java
+++ src/main/java/net/ramen5914/yumby/datagen/ModRecipeProvider.java
@@ -2,21 +2,20 @@ package net.ramen5914.yumby.datagen;
 
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.world.item.Items;
 import net.neoforged.neoforge.common.conditions.IConditionBuilder;
 import net.ramen5914.yumby.Yumby;
+import net.ramen5914.yumby.block.ModBlocks;
 
 import java.util.concurrent.CompletableFuture;
 
 public class ModRecipeProvider extends RecipeProvider implements IConditionBuilder {
     public ModRecipeProvider(HolderLookup.Provider lookupProvider, RecipeOutput recipeOutput) {
         super(lookupProvider, recipeOutput);
-    }
-
-    @Override
-    protected void buildRecipes() {
-
     }
 
     public static final class Runner extends RecipeProvider.Runner {
@@ -33,5 +32,18 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
         public String getName() {
             return Yumby.MOD_ID + " Recipes";
         }
+    }
+
+    @Override
+    protected void buildRecipes() {
+        shaped(RecipeCategory.DECORATIONS, ModBlocks.POT)
+                .pattern("L L")
+                .pattern("I I")
+                .pattern("III")
+                .define('L', Items.LEATHER)
+                .define('I', Items.IRON_INGOT)
+                .unlockedBy("has_iron_ingot", has(Items.IRON_INGOT))
+                .unlockedBy("has_leather", has(Items.LEATHER))
+                .save(this.output);
     }
 }
