a111d4d5b00f8c91fd15adc0fc8e09cbb2d02531
Omar Rahman <omar+github@ramen5914.net>
2024-11-10 08:11:35 -0800

    Bowl block prepped for rotation later

--- src/main/java/net/ramen5914/yumby/block/custom/BowlBlock.java
+++ src/main/java/net/ramen5914/yumby/block/custom/BowlBlock.java
@@ -1,11 +1,25 @@
 package net.ramen5914.yumby.block.custom;
 
+import com.mojang.datafixers.util.Pair;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.DynamicOps;
 import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.SimpleMapCodec;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 
-public class BowlBlock extends Block{
+public class BowlBlock extends RotatedPillarBlock {
+    MapCodec<BowlBlock> CODEC = simpleCodec(BowlBlock::new);
+
     public BowlBlock(BlockBehaviour.Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public MapCodec<BowlBlock> codec() {
+        return CODEC;
     }
 }
