d85754ed5423e6405e015982bd25b564887f72f7
Omar Rahman <omar+github@ramen5914.net>
2024-11-12 04:42:28 -0800

    Made custom function for bowl model generation

--- src/main/java/net/ramen5914/yumby/datagen/ModBlockModelProvider.java
+++ src/main/java/net/ramen5914/yumby/datagen/ModBlockModelProvider.java
@@ -1,6 +1,7 @@
 package net.ramen5914.yumby.datagen;
 
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
 import net.neoforged.neoforge.client.model.generators.BlockModelProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.ramen5914.yumby.Yumby;
@@ -12,35 +13,21 @@ public class ModBlockModelProvider extends BlockModelProvider {
 
     @Override
     protected void registerModels() {
-        withExistingParent("dark_oak_bowl", modLoc("block/bowl"))
-                .texture("material", mcLoc("block/dark_oak_log"));
+        bowl("oak_bowl", mcLoc("block/oak_log"));
+        bowl("dark_oak_bowl", mcLoc("block/dark_oak_log"));
+        bowl("spruce_bowl", mcLoc("block/spruce_log"));
+        bowl("birch_bowl", mcLoc("block/birch_log"));
+        bowl("jungle_bowl", mcLoc("block/jungle_log"));
+        bowl("acacia_bowl", mcLoc("block/acacia_log"));
+        bowl("dark_oak_bowl", mcLoc("block/dark_oak_log"));
+        bowl("mangrove_bowl", mcLoc("block/mangrove_log"));
+        bowl("cherry_bowl", mcLoc("block/cherry_log"));
+        bowl("crimson_bowl", mcLoc("block/crimson_stem"));
+        bowl("warped_bowl", mcLoc("block/warped_stem"));
+    }
 
-        withExistingParent("spruce_bowl", modLoc("block/bowl"))
-                .texture("material", mcLoc("block/spruce_log"));
-
-        withExistingParent("birch_bowl", modLoc("block/bowl"))
-                .texture("material", mcLoc("block/birch_log"));
-
-        withExistingParent("jungle_bowl", modLoc("block/bowl"))
-                .texture("material", mcLoc("block/jungle_log"));
-
-        withExistingParent("acacia_bowl", modLoc("block/bowl"))
-                .texture("material", mcLoc("block/acacia_log"));
-
-        withExistingParent("dark_oak_bowl", modLoc("block/bowl"))
-                .texture("material", mcLoc("block/dark_oak_log"));
-
-        withExistingParent("mangrove_bowl", modLoc("block/bowl"))
-                .texture("material", mcLoc("block/mangrove_log"));
-
-        withExistingParent("cherry_bowl", modLoc("block/bowl"))
-                .texture("material", mcLoc("block/cherry_log"));
-
-        withExistingParent("crimson_bowl", modLoc("block/bowl"))
-                .texture("material", mcLoc("block/crimson_stem"));
-
-        withExistingParent("warped_bowl", modLoc("block/bowl"))
-                .texture("material", mcLoc("block/warped_stem"));
-
+    private void bowl(String name, ResourceLocation material) {
+        withExistingParent(name, modLoc("block/bowl"))
+                .texture("material", material);
     }
 }
