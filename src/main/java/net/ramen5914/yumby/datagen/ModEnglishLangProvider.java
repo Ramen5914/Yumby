6ca1df45283e10ae6cd4c8d4cfc408ab9ba49224
Omar Rahman <omar+github@ramen5914.net>
2024-11-12 04:07:09 -0800

    Translated bowls

--- src/main/java/net/ramen5914/yumby/datagen/ModEnglishLangProvider.java
+++ src/main/java/net/ramen5914/yumby/datagen/ModEnglishLangProvider.java
@@ -15,8 +15,18 @@ public class ModEnglishLangProvider extends LanguageProvider {
     @Override
     protected void addTranslations() {
         addBlockAndItem(ModBlocks.POT, "Pot");
-        addBlockAndItem(ModBlocks.BOWL, "Bowl");
         addBlockAndItem(ModBlocks.PAN, "Pan");
+
+        addBlockAndItem(ModBlocks.OAK_BOWL, "Oak Bowl");
+        addBlockAndItem(ModBlocks.SPRUCE_BOWL, "Spruce Bowl");
+        addBlockAndItem(ModBlocks.BIRCH_BOWL, "Birch Bowl");
+        addBlockAndItem(ModBlocks.JUNGLE_BOWL, "Jungle Bowl");
+        addBlockAndItem(ModBlocks.ACACIA_BOWL, "Acacia Bowl");
+        addBlockAndItem(ModBlocks.DARK_OAK_BOWL, "Dark Oak Bowl");
+        addBlockAndItem(ModBlocks.MANGROVE_BOWL, "Mangrove Bowl");
+        addBlockAndItem(ModBlocks.CHERRY_BOWL, "Cherry Bowl");
+        addBlockAndItem(ModBlocks.CRIMSON_BOWL, "Crimson Bowl");
+        addBlockAndItem(ModBlocks.WARPED_BOWL, "Warped Bowl");
 
         add(String.format("itemgroup.%s.yumby_tab", Yumby.MOD_ID), "Yumby");
     }
