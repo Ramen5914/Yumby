05bca191fce6c249b62ae2a8d6830ce4c5694d42
Omar Rahman <omar+github@ramen5914.net>
2024-11-12 04:06:48 -0800

    Set models for bowls

--- src/main/java/net/ramen5914/yumby/datagen/ModBlockModelProvider.java
+++ src/main/java/net/ramen5914/yumby/datagen/ModBlockModelProvider.java
@@ -1,4 +1,46 @@
 package net.ramen5914.yumby.datagen;
 
-public class ModBlockModelProvider {
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.client.model.generators.BlockModelProvider;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.ramen5914.yumby.Yumby;
+
+public class ModBlockModelProvider extends BlockModelProvider {
+    public ModBlockModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
+        super(output, Yumby.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+        withExistingParent("dark_oak_bowl", modLoc("block/bowl"))
+                .texture("material", mcLoc("block/dark_oak_log"));
+
+        withExistingParent("spruce_bowl", modLoc("block/bowl"))
+                .texture("material", mcLoc("block/spruce_log"));
+
+        withExistingParent("birch_bowl", modLoc("block/bowl"))
+                .texture("material", mcLoc("block/birch_log"));
+
+        withExistingParent("jungle_bowl", modLoc("block/bowl"))
+                .texture("material", mcLoc("block/jungle_log"));
+
+        withExistingParent("acacia_bowl", modLoc("block/bowl"))
+                .texture("material", mcLoc("block/acacia_log"));
+
+        withExistingParent("dark_oak_bowl", modLoc("block/bowl"))
+                .texture("material", mcLoc("block/dark_oak_log"));
+
+        withExistingParent("mangrove_bowl", modLoc("block/bowl"))
+                .texture("material", mcLoc("block/mangrove_log"));
+
+        withExistingParent("cherry_bowl", modLoc("block/bowl"))
+                .texture("material", mcLoc("block/cherry_log"));
+
+        withExistingParent("crimson_bowl", modLoc("block/bowl"))
+                .texture("material", mcLoc("block/crimson_stem"));
+
+        withExistingParent("warped_bowl", modLoc("block/bowl"))
+                .texture("material", mcLoc("block/warped_stem"));
+
+    }
 }
