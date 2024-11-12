71c03581d393358830f5f6a2adca128a3ba800e1
Omar Rahman <omar+github@ramen5914.net>
2024-11-12 04:05:49 -0800

    Adjusted hitbox of bowl item

--- src/main/java/net/ramen5914/yumby/block/custom/BowlBlock.java
+++ src/main/java/net/ramen5914/yumby/block/custom/BowlBlock.java
@@ -6,16 +6,34 @@ import com.mojang.serialization.DataResult;
 import com.mojang.serialization.DynamicOps;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.SimpleMapCodec;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class BowlBlock extends RotatedPillarBlock {
     MapCodec<BowlBlock> CODEC = simpleCodec(BowlBlock::new);
 
+    private static final VoxelShape SHAPE = Block.box(3.5, 0, 3.5, 12.5, 5, 12.5);
+
     public BowlBlock(BlockBehaviour.Properties properties) {
         super(properties);
+    }
+
+    @Override
+    protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return SHAPE;
+    }
+
+    @Override
+    protected RenderShape getRenderShape(BlockState state) {
+        return RenderShape.MODEL;
     }
 
     @Override
