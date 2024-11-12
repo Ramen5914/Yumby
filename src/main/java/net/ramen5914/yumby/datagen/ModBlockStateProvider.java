a51b716b1cdc2ac4aa94899771342c51ed2c8467
Omar Rahman <omar+github@ramen5914.net>
2024-11-12 04:07:00 -0800

    Set blockItem and item models

--- src/main/java/net/ramen5914/yumby/datagen/ModBlockStateProvider.java
+++ src/main/java/net/ramen5914/yumby/datagen/ModBlockStateProvider.java
@@ -17,10 +17,37 @@ public class ModBlockStateProvider extends BlockStateProvider {
         simpleBlockWithItem(ModBlocks.POT.get(),
                 new ModelFile.UncheckedModelFile(modLoc("block/pot")));
 
-//        simpleBlockWithItem(ModBlocks.PAN.get(),
-//                new ModelFile.UncheckedModelFile(modLoc("block/pan")));
-
         horizontalBlock(ModBlocks.PAN.get(),
                  new ModelFile.UncheckedModelFile(modLoc("block/pan")));
+
+        simpleBlockWithItem(ModBlocks.OAK_BOWL.get(),
+                new ModelFile.UncheckedModelFile(modLoc("block/oak_bowl")));
+
+        simpleBlockWithItem(ModBlocks.SPRUCE_BOWL.get(),
+                new ModelFile.UncheckedModelFile(modLoc("block/spruce_bowl")));
+
+        simpleBlockWithItem(ModBlocks.BIRCH_BOWL.get(),
+                new ModelFile.UncheckedModelFile(modLoc("block/birch_bowl")));
+
+        simpleBlockWithItem(ModBlocks.JUNGLE_BOWL.get(),
+                new ModelFile.UncheckedModelFile(modLoc("block/jungle_bowl")));
+
+        simpleBlockWithItem(ModBlocks.ACACIA_BOWL.get(),
+                new ModelFile.UncheckedModelFile(modLoc("block/acacia_bowl")));
+
+        simpleBlockWithItem(ModBlocks.DARK_OAK_BOWL.get(),
+                new ModelFile.UncheckedModelFile(modLoc("block/dark_oak_bowl")));
+
+        simpleBlockWithItem(ModBlocks.MANGROVE_BOWL.get(),
+                new ModelFile.UncheckedModelFile(modLoc("block/mangrove_bowl")));
+
+        simpleBlockWithItem(ModBlocks.CHERRY_BOWL.get(),
+                new ModelFile.UncheckedModelFile(modLoc("block/cherry_bowl")));
+
+        simpleBlockWithItem(ModBlocks.CRIMSON_BOWL.get(),
+                new ModelFile.UncheckedModelFile(modLoc("block/crimson_bowl")));
+
+        simpleBlockWithItem(ModBlocks.WARPED_BOWL.get(),
+                new ModelFile.UncheckedModelFile(modLoc("block/warped_bowl")));
     }
 }
