e7f0161e9cb8997df7deebccf73a92998eedbca2
Omar Rahman <omar+github@ramen5914.net>
2024-12-02 00:57:06 -0800

    Cutting board now rotates and is a proper block entity

--- src/main/java/net/ramen5914/yumby/block/custom/CuttingBoardBlock.java
+++ src/main/java/net/ramen5914/yumby/block/custom/CuttingBoardBlock.java
@@ -3,17 +3,33 @@ package net.ramen5914.yumby.block.custom;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.Containers;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import net.ramen5914.yumby.block.entity.custom.CuttingBoardBlockEntity;
 import org.jetbrains.annotations.Nullable;
 
 public class CuttingBoardBlock extends BaseEntityBlock {
     public static final MapCodec<CuttingBoardBlock> CODEC = simpleCodec(CuttingBoardBlock::new);
+
+    public static final VoxelShape NS_SHAPE = Block.box(2, 0, 3.5, 14, 1, 12.5);
+    public static final VoxelShape EW_SHAPE = Block.box(3.5, 0, 2, 12.5, 1, 14);
 
     public static final EnumProperty<Direction> FACING = BlockStateProperties.HORIZONTAL_FACING;
 
@@ -32,8 +48,18 @@ public class CuttingBoardBlock extends BaseEntityBlock {
     }
 
     @Override
+    protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        Direction direction = state.getValue(FACING);
+        if (direction == Direction.EAST || direction == Direction.WEST) {
+            return EW_SHAPE;
+        } else {
+            return NS_SHAPE;
+        }
+    }
+
+    @Override
     public @Nullable BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
-        return null;
+        return new CuttingBoardBlockEntity(blockPos, blockState);
     }
 
     @Override
@@ -54,5 +80,37 @@ public class CuttingBoardBlock extends BaseEntityBlock {
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
         builder.add(FACING);
+    }
+
+    @Override
+    protected void onRemove(BlockState state, Level level, BlockPos pos, BlockState newState, boolean movedByPiston) {
+        if (state.getBlock() != newState.getBlock()) {
+            if (level.getBlockEntity(pos) instanceof CuttingBoardBlockEntity cuttingBoardBlockEntity) {
+                Containers.dropContents(level, pos, cuttingBoardBlockEntity);
+                level.updateNeighbourForOutputSignal(pos, this);
+            }
+        }
+
+        super.onRemove(state, level, pos, newState, movedByPiston);
+    }
+
+    @Override
+    protected InteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
+        if (level.getBlockEntity(pos) instanceof CuttingBoardBlockEntity cuttingBoardBlockEntity) {
+            if (cuttingBoardBlockEntity.isEmpty() && !stack.isEmpty()) {
+                cuttingBoardBlockEntity.setItem(0, stack);
+                stack.shrink(1);
+
+                level.playSound(player, pos, SoundEvents.ITEM_PICKUP, SoundSource.BLOCKS, 1f, 2f);
+            } else if (!cuttingBoardBlockEntity.isEmpty() && stack.isEmpty()) {
+                ItemStack stackInPot = cuttingBoardBlockEntity.getItem(0);
+                player.setItemInHand(InteractionHand.MAIN_HAND, stackInPot);
+                cuttingBoardBlockEntity.clearContent();
+
+                level.playSound(player, pos, SoundEvents.ITEM_PICKUP, SoundSource.BLOCKS, 1f, 1f);
+            }
+        }
+
+        return InteractionResult.SUCCESS;
     }
 }
