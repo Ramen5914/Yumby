101585fd3801880cdaa41c5706fd60e3bb497e7c
Omar Rahman <omar+github@ramen5914.net>
2024-12-02 00:57:45 -0800

    Created CuttingBoardBlockEntity

--- src/main/java/net/ramen5914/yumby/block/entity/custom/CuttingBoardBlockEntity.java
+++ src/main/java/net/ramen5914/yumby/block/entity/custom/CuttingBoardBlockEntity.java
@@ -1,4 +1,104 @@
 package net.ramen5914.yumby.block.entity.custom;
 
-public class CuttingBoardBlockEntity {
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.NonNullList;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
+import net.minecraft.world.Container;
+import net.minecraft.world.ContainerHelper;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.ramen5914.yumby.block.entity.ModBlockEntities;
+import org.jetbrains.annotations.Nullable;
+
+public class CuttingBoardBlockEntity extends BlockEntity implements Container {
+    private final NonNullList<ItemStack> inventory = NonNullList.withSize(1, ItemStack.EMPTY);
+
+    public CuttingBoardBlockEntity(BlockPos pos, BlockState blockState) {
+        super(ModBlockEntities.CUTTING_BOARD_BE.get(), pos, blockState);
+    }
+
+    @Override
+    public int getContainerSize() {
+        return inventory.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        for (int i = 0; i < getContainerSize(); i++) {
+            ItemStack stack = getItem(i);
+            if (!stack.isEmpty()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public ItemStack getItem(int slot) {
+        setChanged();
+        return inventory.get(slot);
+    }
+
+    @Override
+    public ItemStack removeItem(int slot, int amount) {
+        setChanged();
+        ItemStack stack = inventory.get(slot);
+        stack.shrink(amount);
+
+        return inventory.set(slot, stack);
+    }
+
+    @Override
+    public ItemStack removeItemNoUpdate(int slot) {
+        setChanged();
+
+        return ContainerHelper.takeItem(inventory, slot);
+    }
+
+    @Override
+    public void setItem(int slot, ItemStack stack) {
+        setChanged();
+        inventory.set(slot, stack.copyWithCount(1));
+    }
+
+    @Override
+    public boolean stillValid(Player player) {
+        return Container.stillValidBlockEntity(this, player);
+    }
+
+    @Override
+    public void clearContent() {
+        inventory.clear();
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag tag, HolderLookup.Provider registries) {
+        super.saveAdditional(tag, registries);
+
+        ContainerHelper.saveAllItems(tag, inventory, registries);
+    }
+
+    @Override
+    protected void loadAdditional(CompoundTag tag, HolderLookup.Provider registries) {
+        super.loadAdditional(tag, registries);
+
+        ContainerHelper.loadAllItems(tag, inventory, registries);
+    }
+
+    @Override
+    public @Nullable Packet<ClientGamePacketListener> getUpdatePacket() {
+        return ClientboundBlockEntityDataPacket.create(this);
+    }
+
+    @Override
+    public CompoundTag getUpdateTag(HolderLookup.Provider registries) {
+        return saveWithoutMetadata(registries);
+    }
 }
