6d96b06a25d217eecbdaae00b678369919d470bf
Omar Rahman <omar+github@ramen5914.net>
2024-12-07 06:27:32 -0800

    Fixed the recipe provider to work in 1.21.1

--- src/main/java/net/ramen5914/yumby/datagen/DataGenerator.java
+++ src/main/java/net/ramen5914/yumby/datagen/DataGenerator.java
@@ -30,7 +30,7 @@ public class DataGenerator {
         BlockTagsProvider blockTagsProvider = new ModBlockTagProvider(packOutput, lookupProvider, existingFileHelper);
         generator.addProvider(event.includeServer(), blockTagsProvider);
         generator.addProvider(event.includeServer(), new ModItemTagProvider(packOutput, lookupProvider, blockTagsProvider.contentsGetter(), existingFileHelper));
-        generator.addProvider(event.includeServer(), new ModRecipeProvider.Runner(packOutput, lookupProvider));
+        generator.addProvider(event.includeServer(), new ModRecipeProvider(packOutput, lookupProvider));
         generator.addProvider(event.includeServer(), new LootTableProvider(packOutput, Collections.emptySet(), List.of(
                 new LootTableProvider.SubProviderEntry(ModBlockLootTables::new, LootContextParamSets.BLOCK),
                 new LootTableProvider.SubProviderEntry(ModEntityLootTables::new, LootContextParamSets.ENTITY)
