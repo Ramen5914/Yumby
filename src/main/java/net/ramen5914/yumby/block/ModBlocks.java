a841d8291158ccee3fb6785a40cbf360e8595780
Omar Rahman <omar+github@ramen5914.net>
2024-12-07 06:27:13 -0800

    No Longer need resource keys in 1.21.1

--- src/main/java/net/ramen5914/yumby/block/ModBlocks.java
+++ src/main/java/net/ramen5914/yumby/block/ModBlocks.java
@@ -1,8 +1,5 @@
 package net.ramen5914.yumby.block;
 
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
@@ -17,149 +14,135 @@ import net.ramen5914.yumby.block.custom.PanBlock;
 import net.ramen5914.yumby.block.custom.PotBlock;
 import net.ramen5914.yumby.item.ModItems;
 
-import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class ModBlocks {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(Yumby.MOD_ID);
 
     public static DeferredBlock<Block> POT = registerBlock("pot",
-            (properties) -> new PotBlock(properties
+            () -> new PotBlock(BlockBehaviour.Properties.of()
                     .noOcclusion()),
-            (properties) -> properties
-                    .stacksTo(1));
+                    new Item.Properties()
+                            .stacksTo(1));
 
     public static DeferredBlock<Block> PAN = registerBlock("pan",
-            (properties) -> new PanBlock(properties
+            () -> new PanBlock(BlockBehaviour.Properties.of()
                     .noOcclusion()),
-            (properties) -> properties
-                    .stacksTo(1));
+                    new Item.Properties()
+                            .stacksTo(1));
 
     public static DeferredBlock<Block> OAK_BOWL = registerBlock("oak_bowl",
-            (properties) -> new BowlBlock(properties
-                    .noOcclusion()),
-            (properties) -> properties);
+            () -> new BowlBlock(BlockBehaviour.Properties.of()
+                    .noOcclusion()));
 
     public static DeferredBlock<Block> SPRUCE_BOWL = registerBlock("spruce_bowl",
-            (properties) -> new BowlBlock(properties
-                    .noOcclusion()),
-            (properties) -> properties);
+            () -> new BowlBlock(BlockBehaviour.Properties.of()
+                    .noOcclusion()));
 
     public static DeferredBlock<Block> BIRCH_BOWL = registerBlock("birch_bowl",
-            (properties) -> new BowlBlock(properties
-                    .noOcclusion()),
-            (properties) -> properties);
+            () -> new BowlBlock(BlockBehaviour.Properties.of()
+                    .noOcclusion()));
 
     public static DeferredBlock<Block> JUNGLE_BOWL = registerBlock("jungle_bowl",
-            (properties) -> new BowlBlock(properties
-                    .noOcclusion()),
-            (properties) -> properties);
+            () -> new BowlBlock(BlockBehaviour.Properties.of()
+                    .noOcclusion()));
 
     public static DeferredBlock<Block> ACACIA_BOWL = registerBlock("acacia_bowl",
-            (properties) -> new BowlBlock(properties
-                    .noOcclusion()),
-            (properties) -> properties);
+            () -> new BowlBlock(BlockBehaviour.Properties.of()
+                    .noOcclusion()));
 
     public static DeferredBlock<Block> DARK_OAK_BOWL = registerBlock("dark_oak_bowl",
-            (properties) -> new BowlBlock(properties
-                    .noOcclusion()),
-            (properties) -> properties);
+            () -> new BowlBlock(BlockBehaviour.Properties.of()
+                    .noOcclusion()));
 
     public static DeferredBlock<Block> MANGROVE_BOWL = registerBlock("mangrove_bowl",
-            (properties) -> new BowlBlock(properties
-                    .noOcclusion()),
-            (properties) -> properties);
+            () -> new BowlBlock(BlockBehaviour.Properties.of()
+                    .noOcclusion()));
 
     public static DeferredBlock<Block> CHERRY_BOWL = registerBlock("cherry_bowl",
-            (properties) -> new BowlBlock(properties
-                    .noOcclusion()),
-            (properties) -> properties);
+            () -> new BowlBlock(BlockBehaviour.Properties.of()
+                    .noOcclusion()));
 
     public static DeferredBlock<Block> CRIMSON_BOWL = registerBlock("crimson_bowl",
-            (properties) -> new BowlBlock(properties
-                    .noOcclusion()),
-            (properties) -> properties);
+            () -> new BowlBlock(BlockBehaviour.Properties.of()
+                    .noOcclusion()));
 
     public static DeferredBlock<Block> WARPED_BOWL = registerBlock("warped_bowl",
-            (properties) -> new BowlBlock(properties
-                    .noOcclusion()),
-            (properties) -> properties);
+            () -> new BowlBlock(BlockBehaviour.Properties.of()
+                    .noOcclusion()));
 
     public static DeferredBlock<Block> BAMBOO_BOWL = registerBlock("bamboo_bowl",
-            (properties) -> new BowlBlock(properties
-                    .noOcclusion()),
-            (properties) -> properties);
+            () -> new BowlBlock(BlockBehaviour.Properties.of()
+                    .noOcclusion()));
 
     public static DeferredBlock<Block> OAK_CUTTING_BOARD = registerBlock("oak_cutting_board",
-            (properties) -> new CuttingBoardBlock(properties
-                    .noOcclusion()),
-            (properties) -> properties);
+            () -> new CuttingBoardBlock(BlockBehaviour.Properties.of()
+                    .noOcclusion()));
 
     public static DeferredBlock<Block> SPRUCE_CUTTING_BOARD = registerBlock("spruce_cutting_board",
-            (properties) -> new CuttingBoardBlock(properties
-                    .noOcclusion()),
-            (properties) -> properties);
+            () -> new CuttingBoardBlock(BlockBehaviour.Properties.of()
+                    .noOcclusion()));
 
     public static DeferredBlock<Block> BIRCH_CUTTING_BOARD = registerBlock("birch_cutting_board",
-            (properties) -> new CuttingBoardBlock(properties
-                    .noOcclusion()),
-            (properties) -> properties);
+            () -> new CuttingBoardBlock(BlockBehaviour.Properties.of()
+                    .noOcclusion()));
 
     public static DeferredBlock<Block> JUNGLE_CUTTING_BOARD = registerBlock("jungle_cutting_board",
-            (properties) -> new CuttingBoardBlock(properties
-                    .noOcclusion()),
-            (properties) -> properties);
+            () -> new CuttingBoardBlock(BlockBehaviour.Properties.of()
+                    .noOcclusion()));
 
     public static DeferredBlock<Block> ACACIA_CUTTING_BOARD = registerBlock("acacia_cutting_board",
-            (properties) -> new CuttingBoardBlock(properties
-                    .noOcclusion()),
-            (properties) -> properties);
+            () -> new CuttingBoardBlock(BlockBehaviour.Properties.of()
+                    .noOcclusion()));
 
     public static DeferredBlock<Block> DARK_OAK_CUTTING_BOARD = registerBlock("dark_oak_cutting_board",
-            (properties) -> new CuttingBoardBlock(properties
-                    .noOcclusion()),
-            (properties) -> properties);
+            () -> new CuttingBoardBlock(BlockBehaviour.Properties.of()
+                    .noOcclusion()));
 
     public static DeferredBlock<Block> MANGROVE_CUTTING_BOARD = registerBlock("mangrove_oak_cutting_board",
-            (properties) -> new CuttingBoardBlock(properties
-                    .noOcclusion()),
-            (properties) -> properties);
+            () -> new CuttingBoardBlock(BlockBehaviour.Properties.of()
+                    .noOcclusion()));
 
     public static DeferredBlock<Block> CHERRY_CUTTING_BOARD = registerBlock("cherry_cutting_board",
-            (properties) -> new CuttingBoardBlock(properties
-                    .noOcclusion()),
-            (properties) -> properties);
+            () -> new CuttingBoardBlock(BlockBehaviour.Properties.of()
+                    .noOcclusion()));
 
     public static DeferredBlock<Block> CRIMSON_CUTTING_BOARD = registerBlock("crimson_cutting_board",
-            (properties) -> new CuttingBoardBlock(properties
-                    .noOcclusion()),
-            (properties) -> properties);
+            () -> new CuttingBoardBlock(BlockBehaviour.Properties.of()
+                    .noOcclusion()));
 
     public static DeferredBlock<Block> WARPED_CUTTING_BOARD = registerBlock("warped_cutting_board",
-            (properties) -> new CuttingBoardBlock(properties
-                    .noOcclusion()),
-            (properties) -> properties);
+            () -> new CuttingBoardBlock(BlockBehaviour.Properties.of()
+                    .noOcclusion()));
 
     public static DeferredBlock<Block> BAMBOO_CUTTING_BOARD = registerBlock("bamboo_cutting_board",
-            (properties) -> new CuttingBoardBlock(properties
-                    .noOcclusion()),
-            (properties) -> properties);
+            () -> new CuttingBoardBlock(BlockBehaviour.Properties.of()
+                    .noOcclusion()));
             
 //    public static DeferredBlock<Block> ONION_CROP = registerBlock("onion_crop",
 //            (properties) -> new OnionCropBlock(properties),
 //            (properties) -> properties);
 
-    private static <T extends Block> DeferredBlock<T> registerBlock(String name, Function<BlockBehaviour.Properties, T> blockSupplier, Function<Item.Properties, Item.Properties> itemSupplier) {
-        BlockBehaviour.Properties blockProperties = BlockBehaviour.Properties.of().setId(ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(Yumby.MOD_ID, name)));
-        Item.Properties itemProperties = new Item.Properties().setId(ResourceKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(Yumby.MOD_ID, name)));
+    private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block, Item.Properties itemProperties) {
+        DeferredBlock<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, itemProperties);
 
-        DeferredBlock<T> toReturn = BLOCKS.register(name, () -> blockSupplier.apply(blockProperties));
-        registerBlockItem(name, toReturn, itemSupplier.apply(itemProperties));
+        return toReturn;
+    }
+
+    private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
+        DeferredBlock<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
 
         return toReturn;
     }
 
     private static <T extends Block> void registerBlockItem(String name, DeferredBlock<T> block, Item.Properties properties) {
         ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), properties));
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, DeferredBlock<T> block) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
     }
 
     public static void register(IEventBus eventBus) {
--- src/main/java/net/ramen5914/yumby/item/ModItems.java
+++ src/main/java/net/ramen5914/yumby/item/ModItems.java
@@ -1,9 +1,5 @@
 package net.ramen5914.yumby.item;
 
-import net.minecraft.core.Registry;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
@@ -11,41 +7,38 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 import net.ramen5914.yumby.Yumby;
 import net.ramen5914.yumby.item.custom.KnifeItem;
 
-import java.util.function.Function;
-
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Yumby.MOD_ID);
 
-    public static final DeferredItem<Item> CHEFS_KNIFE = registerItem("chefs_knife",
-            (properties) -> new KnifeItem(properties
-                    .stacksTo(1)));
+    public static final DeferredItem<Item> CHEFS_KNIFE = ITEMS.registerItem("chefs_knife",
+            KnifeItem::new,
+            new Item.Properties()
+                    .stacksTo(1));
 
-    public static final DeferredItem<Item> BUTCHERING_KNIFE = registerItem("butchering_knife",
-            (properties) -> new KnifeItem(properties
-                    .stacksTo(1)));
+    public static final DeferredItem<Item> BUTCHERING_KNIFE = ITEMS.registerItem("butchering_knife",
+            KnifeItem::new,
+            new Item.Properties()
+                    .stacksTo(1));
 
-    public static final DeferredItem<Item> FILLET_KNIFE = registerItem("fillet_knife",
-            (properties -> new KnifeItem(properties
-                    .stacksTo(1))));
+    public static final DeferredItem<Item> FILLET_KNIFE = ITEMS.registerItem("fillet_knife",
+            KnifeItem::new,
+            new Item.Properties()
+                    .stacksTo(1));
 
-    public static final DeferredItem<Item> BEEF_BONE = registerItem("beef_bone", Item::new);
+    public static final DeferredItem<Item> BEEF_BONE = ITEMS.registerItem("beef_bone",
+            Item::new,
+            new Item.Properties());
 
-    public static final DeferredItem<Item> CHICKEN_BONE = registerItem("chicken_bone", Item::new);
+    public static final DeferredItem<Item> CHICKEN_BONE = ITEMS.registerItem("chicken_bone",
+            Item::new,
+            new Item.Properties());
 
-    public static final DeferredItem<Item> PORK_BONE = registerItem("pork_bone", Item::new);
+    public static final DeferredItem<Item> PORK_BONE = ITEMS.registerItem("pork_bone",
+            Item::new,
+            new Item.Properties());
 
 //    public static final DeferredItem<Item> ONION_SEEDS = registerItem("onion_seeds",
 //            (properties) -> new BlockItem(ModBlocks.ONION_CROP.get(), properties));
-
-    private static ResourceKey<Item> createKey(ResourceKey<? extends Registry<Item>> resourceKey, String id) {
-        return ResourceKey.create(resourceKey, ResourceLocation.fromNamespaceAndPath(Yumby.MOD_ID, id));
-    }
-
-    private static <U extends Item> DeferredItem<Item> registerItem(String name, Function<Item.Properties, U> properties) {
-        Item.Properties itemProperties = new Item.Properties().setId(ResourceKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(Yumby.MOD_ID, name)));
-
-        return ITEMS.register(name, () -> properties.apply(itemProperties));
-    }
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
