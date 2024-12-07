ecafbc29f9d0e1c341a8cfa25099e8f8819c7551
Omar Rahman <omar+github@ramen5914.net>
2024-12-07 06:28:36 -0800

    Changed how recipes are datagen'd for 1.21.1

--- src/main/java/net/ramen5914/yumby/datagen/ModRecipeProvider.java
+++ src/main/java/net/ramen5914/yumby/datagen/ModRecipeProvider.java
@@ -21,13 +21,13 @@ import net.ramen5914.yumby.recipe.boiling.BoilingRecipeBuilder;
 import java.util.concurrent.CompletableFuture;
 
 public class ModRecipeProvider extends RecipeProvider implements IConditionBuilder {
-    public ModRecipeProvider(HolderLookup.Provider lookupProvider, RecipeOutput recipeOutput) {
-        super(lookupProvider, recipeOutput);
+    public ModRecipeProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
+        super(output, registries);
     }
 
     @Override
-    protected void buildRecipes() {
-        shaped(RecipeCategory.DECORATIONS, ModBlocks.POT)
+    protected void buildRecipes(RecipeOutput output) {
+        ShapedRecipeBuilder.shaped(RecipeCategory.DECORATIONS, ModBlocks.POT)
                 .pattern("L L")
                 .pattern("I I")
                 .pattern("III")
@@ -35,86 +35,70 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
                 .define('I', Items.IRON_INGOT)
                 .unlockedBy("has_iron_ingot", has(Items.IRON_INGOT))
                 .unlockedBy("has_leather", has(Items.LEATHER))
-                .save(this.output);
+                .save(output);
 
-        shaped(RecipeCategory.DECORATIONS, ModBlocks.PAN)
+        ShapedRecipeBuilder.shaped(RecipeCategory.DECORATIONS, ModBlocks.PAN)
                 .pattern("  I")
                 .pattern("II ")
                 .pattern("II ")
                 .define('I', Items.IRON_INGOT)
                 .unlockedBy("has_iron_ingot", has(Items.IRON_INGOT))
-                .save(this.output);
+                .save(output);
 
         bowl(ModBlocks.OAK_BOWL, Blocks.OAK_SLAB)
                 .unlockedBy("has_oak_slab", has(Blocks.OAK_SLAB))
-                .save(this.output);
+                .save(output);
 
         bowl(ModBlocks.SPRUCE_BOWL, Blocks.SPRUCE_SLAB)
                 .unlockedBy("has_spruce_slab", has(Blocks.SPRUCE_SLAB))
-                .save(this.output);
+                .save(output);
 
         bowl(ModBlocks.BIRCH_BOWL, Blocks.BIRCH_SLAB)
                 .unlockedBy("has_birch_slab", has(Blocks.BIRCH_SLAB))
-                .save(this.output);
+                .save(output);
 
         bowl(ModBlocks.JUNGLE_BOWL, Blocks.JUNGLE_SLAB)
                 .unlockedBy("has_jungle_slab", has(Blocks.JUNGLE_SLAB))
-                .save(this.output);
+                .save(output);
 
         bowl(ModBlocks.ACACIA_BOWL, Blocks.ACACIA_SLAB)
                 .unlockedBy("has_acacia_slab", has(Blocks.ACACIA_SLAB))
-                .save(this.output);
+                .save(output);
 
         bowl(ModBlocks.DARK_OAK_BOWL, Blocks.DARK_OAK_SLAB)
                 .unlockedBy("has_dark_oak_slab", has(Blocks.DARK_OAK_SLAB))
-                .save(this.output);
+                .save(output);
 
         bowl(ModBlocks.MANGROVE_BOWL, Blocks.MANGROVE_SLAB)
                 .unlockedBy("has_mangrove_slab", has(Blocks.MANGROVE_SLAB))
-                .save(this.output);
+                .save(output);
 
         bowl(ModBlocks.CHERRY_BOWL, Blocks.CHERRY_SLAB)
                 .unlockedBy("has_cherry_slab", has(Blocks.CHERRY_SLAB))
-                .save(this.output);
+                .save(output);
 
         bowl(ModBlocks.CRIMSON_BOWL, Blocks.CRIMSON_SLAB)
                 .unlockedBy("has_crimson_slab", has(Blocks.CRIMSON_SLAB))
-                .save(this.output);
+                .save(output);
 
         bowl(ModBlocks.WARPED_BOWL, Blocks.WARPED_SLAB)
                 .unlockedBy("has_warped_slab", has(Blocks.WARPED_SLAB))
-                .save(this.output);
+                .save(output);
 
         bowl(ModBlocks.BAMBOO_BOWL, Blocks.BAMBOO_SLAB)
                 .unlockedBy("has_stripped_bamboo_block", has(Blocks.BAMBOO_SLAB))
-                .save(this.output);
+                .save(output);
 
         new BoilingRecipeBuilder(new ItemStack(Items.BONE), Ingredient.of(ModItems.BEEF_BONE))
                 .unlockedBy("has_beef_bone", has(ModItems.BEEF_BONE))
                 .group("misc")
-                .save(this.output, ResourceLocation.fromNamespaceAndPath(Yumby.MOD_ID, "bone"));
+                .save(output, ResourceLocation.fromNamespaceAndPath(Yumby.MOD_ID, "bone"));
     }
 
     private ShapedRecipeBuilder bowl(ItemLike output, ItemLike input) {
-        return shaped(RecipeCategory.MISC, output)
+        return ShapedRecipeBuilder.shaped(RecipeCategory.MISC, output)
                 .pattern("M M")
                 .pattern("MMM")
                 .define('M', input);
-    }
-
-    public static final class Runner extends RecipeProvider.Runner {
-        public Runner(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider) {
-            super(output, lookupProvider);
-        }
-
-        @Override
-        protected RecipeProvider createRecipeProvider(HolderLookup.Provider lookupProvider, RecipeOutput output) {
-            return new ModRecipeProvider(lookupProvider, output);
-        }
-
-        @Override
-        public String getName() {
-            return Yumby.MOD_ID + " Recipes";
-        }
     }
 }
