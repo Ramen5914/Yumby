72fa5dd032f9beb0dfb201d6f942aa8d44497d7b
Omar Rahman <omar+github@ramen5914.net>
2024-12-07 06:35:22 -0800

    Added required methods to JEI plugin

--- src/main/java/net/ramen5914/yumby/compat/jei/JeiPlugin.java
+++ src/main/java/net/ramen5914/yumby/compat/jei/JeiPlugin.java
@@ -1,3 +1,13 @@
 package net.ramen5914.yumby.compat.jei;
 
-public class JeiPlugin {}
+import mezz.jei.api.IModPlugin;
+import net.minecraft.resources.ResourceLocation;
+import net.ramen5914.yumby.Yumby;
+
+@mezz.jei.api.JeiPlugin
+public class JeiPlugin implements IModPlugin {
+    @Override
+    public ResourceLocation getPluginUid() {
+        return ResourceLocation.fromNamespaceAndPath(Yumby.MOD_ID, "jei_plugin");
+    }
+}
