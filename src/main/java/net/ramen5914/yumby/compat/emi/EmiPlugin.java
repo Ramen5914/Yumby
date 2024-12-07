1aa301fe147de5d285777ea18cfee74709a562c5
Omar Rahman <omar+github@ramen5914.net>
2024-12-07 06:35:10 -0800

    Made a proper Emi plugin

--- src/main/java/net/ramen5914/yumby/compat/emi/EmiPlugin.java
+++ src/main/java/net/ramen5914/yumby/compat/emi/EmiPlugin.java
@@ -1,13 +1,12 @@
-//package net.ramen5914.yumby.compat.emi;
-//
-//import dev.emi.emi.api.EmiEntrypoint;
-//import dev.emi.emi.api.EmiPlugin;
-//import dev.emi.emi.api.EmiRegistry;
-//
-//@EmiEntrypoint
-//public class EmiPlugin implements EmiPlugin {
-//    @Override
-//    public void register(EmiRegistry registry) {
-//
-//    }
-//}
+package net.ramen5914.yumby.compat.emi;
+
+import dev.emi.emi.api.EmiEntrypoint;
+import dev.emi.emi.api.EmiRegistry;
+
+@EmiEntrypoint
+public class EmiPlugin implements dev.emi.emi.api.EmiPlugin {
+    @Override
+    public void register(EmiRegistry registry) {
+
+    }
+}
