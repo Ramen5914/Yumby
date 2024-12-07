4e56c1f856efc610ecab2ed79dee6fdc5ca7dfd7
Omar Rahman <omar+github@ramen5914.net>
2024-12-07 06:29:08 -0800

    Update BoilingRecipeBuilder.java

--- src/main/java/net/ramen5914/yumby/recipe/boiling/BoilingRecipeBuilder.java
+++ src/main/java/net/ramen5914/yumby/recipe/boiling/BoilingRecipeBuilder.java
@@ -5,10 +5,9 @@ import net.minecraft.advancements.AdvancementRequirements;
 import net.minecraft.advancements.AdvancementRewards;
 import net.minecraft.advancements.critereon.RecipeUnlockedTrigger;
 import net.minecraft.data.recipes.RecipeOutput;
-import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.item.crafting.Recipe;
 import net.ramen5914.yumby.recipe.SimpleRecipeBuilder;
 
 public class BoilingRecipeBuilder extends SimpleRecipeBuilder {
@@ -16,19 +15,18 @@ public class BoilingRecipeBuilder extends SimpleRecipeBuilder {
 
     public BoilingRecipeBuilder(ItemStack result, Ingredient inputItem) {
         super(result);
+
         this.inputItem = inputItem;
     }
 
     @Override
-    public void save(RecipeOutput output, ResourceKey<Recipe<?>> key) {
+    public void save(RecipeOutput output, ResourceLocation id) {
         Advancement.Builder advancement = output.advancement()
-                .addCriterion("has_the_recipe", RecipeUnlockedTrigger.unlocked(key))
-                .rewards(AdvancementRewards.Builder.recipe(key))
+                .addCriterion("has_the_recipe", RecipeUnlockedTrigger.unlocked(id))
+                .rewards(AdvancementRewards.Builder.recipe(id))
                 .requirements(AdvancementRequirements.Strategy.OR);
-
         this.criteria.forEach(advancement::addCriterion);
-
         BoilingRecipe recipe = new BoilingRecipe(this.inputItem, this.result);
-        output.accept(key, recipe, advancement.build(key.location().withPrefix("recipes/")));
+        output.accept(id, recipe, advancement.build(id.withPrefix("recipes/")));
     }
 }
