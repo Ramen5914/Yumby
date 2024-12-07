1f60adfb22b8783dcb3ae503135e9dd36ac4d481
Omar Rahman <omar+github@ramen5914.net>
2024-12-07 06:26:39 -0800

    BlockEntities do not use Sets in mc 1.21.1

--- src/main/java/net/ramen5914/yumby/block/entity/ModBlockEntities.java
+++ src/main/java/net/ramen5914/yumby/block/entity/ModBlockEntities.java
@@ -9,7 +9,6 @@ import net.ramen5914.yumby.block.ModBlocks;
 import net.ramen5914.yumby.block.entity.custom.CuttingBoardBlockEntity;
 import net.ramen5914.yumby.block.entity.custom.PotBlockEntity;
 
-import java.util.Set;
 import java.util.function.Supplier;
 
 public class ModBlockEntities {
@@ -17,27 +16,24 @@ public class ModBlockEntities {
             DeferredRegister.create(BuiltInRegistries.BLOCK_ENTITY_TYPE, Yumby.MOD_ID);
 
     public static final Supplier<BlockEntityType<PotBlockEntity>> POT_BE =
-            BLOCK_ENTITIES.register("pot_be", () -> new BlockEntityType<>(
-                    PotBlockEntity::new,
-                    ModBlocks.POT.get()
-            ));
+            BLOCK_ENTITIES.register("pot_be", () -> BlockEntityType.Builder.of(
+                    PotBlockEntity::new, ModBlocks.POT.get()).build(null));
 
     public static final Supplier<BlockEntityType<CuttingBoardBlockEntity>> CUTTING_BOARD_BE =
-            BLOCK_ENTITIES.register("cutting_board_be", () -> new BlockEntityType<>(
+            BLOCK_ENTITIES.register("cutting_board_be", () -> BlockEntityType.Builder.of(
                     CuttingBoardBlockEntity::new,
-                    Set.of(
-                            ModBlocks.ACACIA_CUTTING_BOARD.get(),
-                            ModBlocks.BAMBOO_CUTTING_BOARD.get(),
-                            ModBlocks.BIRCH_CUTTING_BOARD.get(),
-                            ModBlocks.CHERRY_CUTTING_BOARD.get(),
-                            ModBlocks.CRIMSON_CUTTING_BOARD.get(),
-                            ModBlocks.DARK_OAK_CUTTING_BOARD.get(),
-                            ModBlocks.JUNGLE_CUTTING_BOARD.get(),
-                            ModBlocks.OAK_CUTTING_BOARD.get(),
-                            ModBlocks.SPRUCE_CUTTING_BOARD.get(),
-                            ModBlocks.MANGROVE_CUTTING_BOARD.get(),
-                            ModBlocks.WARPED_CUTTING_BOARD.get())
-            ));
+                    ModBlocks.ACACIA_CUTTING_BOARD.get(),
+                    ModBlocks.BAMBOO_CUTTING_BOARD.get(),
+                    ModBlocks.BIRCH_CUTTING_BOARD.get(),
+                    ModBlocks.CHERRY_CUTTING_BOARD.get(),
+                    ModBlocks.CRIMSON_CUTTING_BOARD.get(),
+                    ModBlocks.DARK_OAK_CUTTING_BOARD.get(),
+                    ModBlocks.JUNGLE_CUTTING_BOARD.get(),
+                    ModBlocks.OAK_CUTTING_BOARD.get(),
+                    ModBlocks.SPRUCE_CUTTING_BOARD.get(),
+                    ModBlocks.MANGROVE_CUTTING_BOARD.get(),
+                    ModBlocks.WARPED_CUTTING_BOARD.get()
+            ).build(null));
 
     public static void register(IEventBus eventBus) {
         BLOCK_ENTITIES.register(eventBus);
