5693a541efc8a3e398a25a8ba4b738167cbdea3c
Omar Rahman <omar+github@ramen5914.net>
2024-12-08 19:18:21 -0800

    All new SteamCodecs and Codecs for serialization and deserialization

--- src/main/java/net/ramen5914/yumby/recipe/boiling/BoilingRecipeSerializer.java
+++ src/main/java/net/ramen5914/yumby/recipe/boiling/BoilingRecipeSerializer.java
@@ -1,7 +1,9 @@
 package net.ramen5914.yumby.recipe.boiling;
 
+import com.mojang.serialization.DataResult;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.core.NonNullList;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.world.item.ItemStack;
@@ -10,16 +12,47 @@ import net.minecraft.world.item.crafting.RecipeSerializer;
 
 public class BoilingRecipeSerializer implements RecipeSerializer<BoilingRecipe> {
     public static final MapCodec<BoilingRecipe> CODEC = RecordCodecBuilder.mapCodec(inst -> inst.group(
-            Ingredient.CODEC.fieldOf("ingredient").forGetter(BoilingRecipe::getInputItem),
-            ItemStack.CODEC.fieldOf("result").forGetter(BoilingRecipe::getResult)
+            ItemStack.STRICT_CODEC.fieldOf("result").forGetter(BoilingRecipe::getResult),
+            Ingredient.CODEC_NONEMPTY
+                    .listOf()
+                    .fieldOf("ingredients")
+                    .flatXmap(
+                            ingredients -> {
+                                Ingredient[] aIngredient = ingredients.toArray(Ingredient[]::new);
+                                if (aIngredient.length == 0) {
+                                    return DataResult.error(() -> "No ingredients for boiling recipe");
+                                } else {
+                                            ? DataResult.error(() -> "Too many ingredients for boiling recipe. The maximus is: %s".formatted(BoilingRecipe.MAX_INGREDIENTS))
+                                            : DataResult.success(NonNullList.of(Ingredient.EMPTY, aIngredient));
+                                }
+                            },
+                            DataResult::success
+                    )
+                    .forGetter(BoilingRecipe::getIngredients)
     ).apply(inst, BoilingRecipe::new));
 
-    public static final StreamCodec<RegistryFriendlyByteBuf, BoilingRecipe> STREAM_CODEC =
-            StreamCodec.composite(
-                    Ingredient.CONTENTS_STREAM_CODEC, BoilingRecipe::getInputItem,
-                    ItemStack.STREAM_CODEC, BoilingRecipe::getResult,
-                    BoilingRecipe::new
-            );
+    public static final StreamCodec<RegistryFriendlyByteBuf, BoilingRecipe> STREAM_CODEC = StreamCodec.of(
+            BoilingRecipeSerializer::toNetwork, BoilingRecipeSerializer::fromNetwork
+    );
+
+    private static void toNetwork(RegistryFriendlyByteBuf buffer, BoilingRecipe recipe) {
+        buffer.writeVarInt(recipe.getIngredients().size());
+
+        for (Ingredient ingredient : recipe.getIngredients()) {
+            Ingredient.CONTENTS_STREAM_CODEC.encode(buffer, ingredient);
+        }
+
+        ItemStack.STREAM_CODEC.encode(buffer, recipe.getResult());
+    }
+
+    private static BoilingRecipe fromNetwork(RegistryFriendlyByteBuf buffer) {
+        int size = buffer.readVarInt();
+        NonNullList<Ingredient> ingredients = NonNullList.withSize(size, Ingredient.EMPTY);
+        ingredients.replaceAll(ingredient -> Ingredient.CONTENTS_STREAM_CODEC.decode(buffer));
+        ItemStack itemStack = ItemStack.STREAM_CODEC.decode(buffer);
+
+        return new BoilingRecipe(itemStack, ingredients);
+    }
 
     @Override
     public MapCodec<BoilingRecipe> codec() {
