7893ca9ed395c1ef66e55dc79dbd9fed0d82988e
Omar Rahman <omar+github@ramen5914.net>
2024-12-08 19:17:56 -0800

    Update BoilingRecipeInput.java

--- src/main/java/net/ramen5914/yumby/recipe/boiling/BoilingRecipeInput.java
+++ src/main/java/net/ramen5914/yumby/recipe/boiling/BoilingRecipeInput.java
@@ -1,17 +1,59 @@
 package net.ramen5914.yumby.recipe.boiling;
 
+import net.minecraft.world.entity.player.StackedContents;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeInput;
 
-public record BoilingRecipeInput(ItemStack stack) implements RecipeInput {
+import java.util.List;
+
+public class BoilingRecipeInput implements RecipeInput {
+    private final List<ItemStack> items;
+    private final StackedContents stackedContents = new StackedContents();
+    private final int ingredientCount;
+
+    public BoilingRecipeInput(List<ItemStack> item) {
+        this.items = item;
+        int i = 0;
+
+        for (ItemStack itemStack : item) {
+            if (!itemStack.isEmpty()) {
+                i++;
+                this.stackedContents.accountStack(itemStack, 1);
+            }
+        }
+
+        this.ingredientCount = i;
+    }
+
     @Override
-    public ItemStack getItem(int slot) {
-        if (slot != 0) throw new IllegalArgumentException("No item for index " + slot);
-        return this.stack();
+    public ItemStack getItem(int index) {
+        return this.items.get(index);
     }
 
     @Override
     public int size() {
-        return 1;
+        return this.items.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.ingredientCount == 0;
+    }
+
+    public StackedContents stackedContents() {
+        return this.stackedContents;
+    }
+
+    public List<ItemStack> items() {
+        return this.items;
+    }
+
+    public int ingredientCount() {
+        return this.ingredientCount;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this;
     }
 }
